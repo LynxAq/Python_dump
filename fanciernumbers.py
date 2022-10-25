b7ccc4288817873fd9a93e46e7fa30fbb0505fa8
LynxAq <115657550+LynxAq@users.noreply.github.com>
2022-10-25 23:15:14 +0200

    Update fanciernumbers.py

--- fanciernumbers.py
+++ fanciernumbers.py
@@ -9,7 +9,7 @@ def fancyfactor(fancy_factor):
                 fancy_number = str(i) + str(j) + str(x) + fancy_factor
                 list.append(int(fancy_number))
 
-    for i in range(1,10):
+    for i in range(0,10):
         for j in range(0,10):
             for x in range(0,10):
                 fancy_number = fancy_factor + str(i) + str(j) + str(x)
@@ -37,17 +37,30 @@ def fancierfactor(fancy_factor):
 
 a = fancyfactor('7777777')
 b = fancierfactor('7777777')
-all_the_right_numbers = a + b
+c = a + b
 
 
-def prime_number_checker(all_the_right_numbers):
+unique = []
+dupes = []
+for i in c:
+    if i not in unique:
+        unique.append(i)
+    else:
+        dupes.append(i)
+print(dupes)
+print(unique)
+
+numeros = dupes + unique
+
+def prime_number_checker(unique):
     amount = 0
-    for i in all_the_right_numbers:
+    for i in unique:
         for x in range(2,int(sqrt(i) + 1)):
             if (i % x) == 0:
                 amount += 1
                 break
-    amount_of_primes = len(all_the_right_numbers) - amount
+    amount_of_primes = len(unique) - amount
     return amount_of_primes
 
-print(prime_number_checker(all_the_right_numbers))
+
+print(prime_number_checker(unique))
